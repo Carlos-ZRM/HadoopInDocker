f69b9c93e9746f682a927fe82f470416c929a897
Carlos-ZRM <carloszrm90@gmail.com>
2018-10-01 06:29:12 -0500

    Fiebre de sabado por la noche

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM centos:6
 
-RUN yum -y install java-1.8.0-openjdk-devel openssh-server openssh-clients wget
+RUN yum -y install java-1.8.0-openjdk-devel openssh-server openssh-clients wget &&\
+	wget http://www-eu.apache.org/dist/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz &&\
+	tar xzf hadoop-2.6.5.tar.gz
+
 EXPOSE 50070 50075 50090 8088 9000
 
 COPY entrypoint.sh .
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,23 @@ chkconfig sshd on
 /etc/init.d/sshd start
 /etc/init.d/sshd status
 
-adduser $HA_USER -p $HA_PASS
+echo "uno"
+useradd $HA_USER -p $HA_PASS
+#su - $HA_USER
+pwd
+su hadoop - -c "ssh-keygen -t rsa -b 4096 -C \"$HA_USER\" -P \"$HA_PASS\" -f \"/home/hadoop/.ssh/id_rsa\""
 
+#ssh-keygen -t rsa -b 4096 -C $HA_USER  -P $HA_PASS
+echo dos
+cat /home/hadoop/.ssh/id_rsa.pub >> /home/hadoop/.ssh/authorized_keys
+echo tres
 
+mv /hadoop-2.6.5 /home/hadoop/hadoop/
 
+#ssh-keygen -t rsa -b 4096 -C "your_email@example.com" -f "/home/hadoop/.ssh/id_rsa" -P "hadoop"
 #passwd hadoop
 #mkdir -p /home/hadoop/.ssh
-touch /home/hadoop/.ssh/authorized_keys
-chmod 700 /home/hadoop/.ssh/
-chmod 600 /home/hadoop/.ssh/authorized_keys
-chown hadoop /home/hadoop/ -R
-
+#touch /home/hadoop/.ssh/authorized_keys
+#chmod 700 /home/hadoop/.ssh/
+#chmod 600 /home/hadoop/.ssh/authorized_keys
+#chown hadoop /home/hadoop/ -R
